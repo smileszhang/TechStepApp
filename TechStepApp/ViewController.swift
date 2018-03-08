ecd31a92364012b5cf6978419a8e4491f7565b1b
smile <zhangshiyang@smile-2.local>
2018-03-07 19:06:18 -0600

    Initial Commit

---
+++ TechStepApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechStepApp
+//
+//  Created by smile on 2018/3/7.
+//  Copyright © 2018年 smile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
